e5a3adaa132e94dffc60383a199412833b53beba
Gustavo Barreto <gustabarreto@gmail.com>
2025-03-16 16:45:09 -0300

    Everything working

--- cypress/e2e/cliente/happyDeletaCliente.cy.js
+++ cypress/e2e/cliente/happyDeletaCliente.cy.js
@@ -2,19 +2,21 @@ describe('Deletar Cliente', () => {
     beforeEach('Acessar a tela de cliente', () => {
         cy.visit("/")
         cy.get('[href="src/consulta.html"]').click()
-        cy.get(":nth-child(14) > :nth-child(7) > a > .btn-danger").click()
     })
-    it('Deve deletar o cliente cadastrado', () => {
-        cy.
-        cy.url().then((url) => {
-            const urlParams = new URLSearchParams(new URL(url).search)
-            const id = urlParams.get("id")
-            cy.log("ID capturado:", id)
-      
-            cy.intercept(
-              "PUT",
-              "/ecommerce_tenis_war_exploded/controlecliente?id=" + id + ""
-            ).as("alterar")
-          });
-    })
-})   
+    it('Deve deletar um cliente cadastrado', () => {
+        cy.get(':nth-child(1) > :nth-child(7) > .btn-danger').click()
+    
+        cy.on("window:confirm", (text) => {
+            expect(text).to.contain("Tem certeza que deseja excluir o cliente com ID 1?")
+            return true;
+        });
+    
+        cy.intercept("DELETE", "/ecommerce_tenis_war_exploded/controlecliente?id=1").as("deleteRequest");
+    
+        // Aguarda a requisição DELETE ser enviada
+        cy.wait("@deleteRequest").then(({ response }) => {
+            expect(response.statusCode).to.eq(200);
+        });
+    });
+    
+})
