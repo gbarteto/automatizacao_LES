38b2f46f5a0bd855031868dbbd33c85b92752835
Gustavo Barreto <gustabarreto987@gmail.com>
2025-05-05 17:46:58 -0300

    Finalizando Troca Cliente

--- cypress/e2e/vendas/happyPedidoCliente.cy.js
+++ cypress/e2e/vendas/happyPedidoCliente.cy.js
@@ -10,8 +10,28 @@ describe('Acessando Pedido e Detalhes', () => {
         cy.get('#cpf-ident').type('71301412040')
         cy.get('#btn-prosseguir').click()
         cy.contains('Meus Pedidos').should('be.visible')
-        cy.get('[data-id="11"] > tbody > tr > :nth-child(4) > .btn-detalhes').click()
+        cy.get('tbody > tr').first().find(':nth-child(4) .btn-detalhes').click();
         cy.contains('Detalhes Pedido').should('be.visible')
+
+        cy.get('#quantidade0').type('1')
+        let alertCount = 0;
+
+        cy.on('window:alert', (str) => {
+        alertCount++;
+
+        if (alertCount === 1) {
+            expect(str).to.equal('Produto "Nike Air Max 90" adicionado para troca.');
+        } else if (alertCount === 2) {
+            expect(str).to.equal('Solicitação de troca enviada com sucesso!');
+        } else {
+            throw new Error('Alerta inesperado: ' + str);
+        }
+        });
+
+        
+
+        cy.get('.solicitar-troca > button').click()
+        cy.get('#finalizarTroca').click()
 
         
     })
