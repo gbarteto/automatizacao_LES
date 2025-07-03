9686f678756591702968d619a833725726f2229d
Gustavo Barreto <gustabarreto987@gmail.com>
2025-07-02 23:06:17 -0300

    Teste de Condução completo

--- cypress/e2e/vendas/happyVendaCliente.cy.js
+++ cypress/e2e/vendas/happyVendaCliente.cy.js
@@ -5,17 +5,7 @@ describe('Venda Cliente - Sucesso', () => {
         cy.contains('SELECIONAR USUÁRIO').should('be.visible')
         cy.get('[href="cliente/clienteCompras.html"]').click()
         cy.get(':nth-child(1) > a > button').click()
-        cy.on('window:alert', (str) => {
-            expect(str).to.equal('Produto adicionado ao carrinho!');
-          });
         cy.get('#add-carrinho').click()
-        cy.saveSessionStorage()
-    });
-
-    it('Deve finalizar uma venda', () => {
-        cy.visit("/src/vendas/cliente/clienteCompras.html")
-        cy.restoreSessionStorage()
-        cy.reload()
         cy.get('[href="clienteCarrinho.html"] > img').click()
         cy.get('.info-carrinho > button').click()
         cy.get('#cpf-ident').type('71301412040')
@@ -24,9 +14,6 @@ describe('Venda Cliente - Sucesso', () => {
         cy.get('.tipo-entrega > [data-id="1"]').click()
         cy.get('#btnProsseguir').click()
         cy.get('.cartao-container').click()
-        cy.on('window:alert', (str) => {
-            expect(str).to.equal('pedido salvo com sucesso!');
-          });
         cy.get('#btnFinalizar').click()
     })
     })
